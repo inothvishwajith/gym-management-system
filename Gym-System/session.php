bba42eb1ec83e9be0faf4962b0984008aa8ade06
Inoth Vishwajith <124657438+inothvishwajith@users.noreply.github.com>
2023-06-26 05:58:23 +0100

    gym commit

---
+++ Gym-System/session.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
+    header("location: index.php");
+    exit();
+}
+$session_id=$_SESSION['user_id'];
+
+?>
---
+++ gymnsb.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2023 at 06:45 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gymnsb (4)`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
+(2, 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `message`, `date`) VALUES
+(10, 'This is a demo announcement from admin', '2022-06-03'),
+(11, 'Gym closed', '2022-09-08'),
+(12, 'Wear face mask in the gym.', '2022-09-10'),
+(13, 'New Belts HERE! ', '2022-09-10'),
+(14, '', '0000-00-00'),
+(15, 'dvffrvfrvfr', '2022-11-23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `curr_date` text NOT NULL,
+  `curr_time` text NOT NULL,
+  `present` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `vendor` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`id`, `name`, `amount`, `quantity`, `vendor`, `description`, `address`, `contact`, `date`) VALUES
+(3, 'Treadmill', 14544, 4, 'DnS', 'Edited Description', 'Colombo 7', '0111479633', '2019-03-07'),
+(4, 'Vertical Press Machine', 55623, 3, 'SS Industries', 'For Biceps And Triceps, Upper Back, Chest', 'Colombo 5', '0115558980', '2020-03-19'),
+(5, 'Dumbell - Adjustable', 68952, 26, 'Uptown Suppliers', 'Material: Steel, Rubber Plastic, Concrete', 'Colombo 7', '0115552100', '2020-03-29'),
+(6, 'Multi Bench Press Machine', 17520, 2, 'DnS Suppliers', '6 In 1 Multi Bench With Incline, Flat, Decline Ben', 'Colombo 5', '0110001010', '2020-04-05'),
+(10, 'RowWarrior Fitness Rowing Mach', 808704, 12, 'Roww Stores', 'HIGHEST QUALITY: This best of class air rowing mac', 'Colombo 5', '0112585555', '2021-06-12'),
+(11, 'Belt', 20000, 20, 'Dns', 'lather waist belts', 'Colombo 7', '0115552104', '2022-09-09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `dor` date NOT NULL,
+  `services` varchar(50) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `paid_date` date NOT NULL,
+  `p_year` int(11) NOT NULL,
+  `plan` varchar(100) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Active',
+  `attendance_count` int(100) NOT NULL,
+  `ini_weight` int(100) NOT NULL DEFAULT 0,
+  `curr_weight` int(100) NOT NULL DEFAULT 0,
+  `ini_bodytype` varchar(50) NOT NULL,
+  `curr_bodytype` varchar(50) NOT NULL,
+  `progress_date` date NOT NULL,
+  `reminder` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`user_id`, `fullname`, `username`, `password`, `gender`, `dor`, `services`, `amount`, `paid_date`, `p_year`, `plan`, `address`, `contact`, `status`, `attendance_count`, `ini_weight`, `curr_weight`, `ini_bodytype`, `curr_bodytype`, `progress_date`, `reminder`) VALUES
+(45, 'Kawishka Roshan', 'roshan', '884ce4bb65d328ecb03c598409e2b168', 'Male', '2022-10-16', 'Fitness', 2000, '2022-11-23', 2022, '1', 'Colombo 5', '0115552100', 'Active', 0, 0, 0, '', '', '2022-11-23', 0),
+(50, 'Dasuni Shanaki', 'dasuni', '6f2268bd1d3d3ebaabb04d6b5d099425', 'Female', '2022-11-23', 'Fitness', 2000, '2022-11-23', 0, '1', 'Matara', '07485963', 'Active', 0, 0, 0, '', '', '0000-00-00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rates`
+--
+
+CREATE TABLE `rates` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `charge` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rates`
+--
+
+INSERT INTO `rates` (`id`, `name`, `charge`) VALUES
+(1, 'Fitness', '2000'),
+(2, 'Sauna', '3000'),
+(3, 'Cardio', '2500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminder`
+--
+
+CREATE TABLE `reminder` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `status` text NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `reminder`
+--
+
+INSERT INTO `reminder` (`id`, `name`, `message`, `status`, `date`, `user_id`) VALUES
+(12, 'staff', 'asd', 'unread', '2020-04-16 22:39:59', 0),
+(13, 'staff', 'asdasdas', 'unread', '2020-04-16 22:40:49', 0),
+(14, 'staff', 'ASasA', 'unread', '2020-04-16 22:41:59', 0),
+(15, 'staff', 'asdasdasd', 'unread', '2020-04-16 22:42:28', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `contact` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `staffs`
+--
+
+INSERT INTO `staffs` (`user_id`, `username`, `password`, `email`, `fullname`, `address`, `designation`, `gender`, `contact`) VALUES
+(1, 'nisal', 'cac29d7a34687eb14b37068ee4708e7b', 'brunoden@mail.com', 'Nisal Tharaka', '26 Main Street', 'Cashier', 'Male', 852028120),
+(2, 'kumara', 'cac29d7a34687eb14b37068ee4708e7b', 'michelle@mail.com', 'Kumara Silva', '61 Pahan Lane', 'Cashier', 'Female', 2147483647),
+(3, 'akash', 'cac29d7a34687eb14b37068ee4708e7b', 'jamesb@mail.com', 'Janith  Akash', '12 newtown', 'Cashier', 'Male', 2147483647),
+(4, 'perera', 'cac29d7a34687eb14b37068ee4708e7b', 'bruce@mail.com', 'Kumudith Perera', 'Colombo 5', 'Cashier', 'Male', 1458887788),
+(5, 'roshan', '202cb962ac59075b964b07152d234b70', 'roshan@gmail.com', 'Kawishka Roshan', 'no.123 newtown', 'Trainer', 'Male', 784512456),
+(6, 'thushara', '8d5e957f297893487bd98fa830fa6413', 'thushara@gmail.com', 'Thushara Locus', 'Colombo 5', 'GYM Assistant', 'Male', 757842159);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `task_status` varchar(50) NOT NULL,
+  `task_desc` varchar(30) NOT NULL,
+  `user_id` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `todo`
+--
+
+INSERT INTO `todo` (`id`, `task_status`, `task_desc`, `user_id`) VALUES
+(20, 'In Progress', 'Test Completed', 14),
+(21, 'Pending', 'Mastering Crunches', 6),
+(22, 'In Progress', 'Standing Workouts For Flat Abs', 6),
+(23, 'In Progress', 'Triceps Buildup - 3 set', 14),
+(24, 'Pending', 'Decline dumbbell bench press', 6),
+(27, 'Pending', 'dddd', 0),
+(28, 'In Progress', 'Test 1', 23),
+(30, 'In Progress', '20 Pushups ', 30),
+(31, 'Pending', '10 dumble pushups', 33);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `rates`
+--
+ALTER TABLE `rates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `rates`
+--
+ALTER TABLE `rates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `reminder`
+--
+ALTER TABLE `reminder`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `staffs`
+--
+ALTER TABLE `staffs`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
